4f34de4fa90307df8d0dddd33fee543e4a2eaea3
Oleg Lashin <legyss7@gmail.com>
2024-08-21 18:57:00 +0600

    HW7

---
+++ SPASample/SPAWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SPAWebApplication.Models;
+
+namespace SPAWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            List<Employee> employee = new List<Employee>();
+            employee.Add(new Employee
+            {
+                Name = "Oleg",
+                Age = 30,
+                Id = 10001
+            });
+            employee.Add(new Employee
+            {
+                Name = "Masha",
+                Age = 34,
+                Id = 10002
+            });
+            employee.Add(new Employee
+            {
+                Name = "Dima",
+                Age = 39,
+                Id = 10003
+            });
+            return View(employee);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ SPASample/SPAWebApplication/Program.cs
@@ -0,0 +1,24 @@
+namespace SPAWebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            //app.MapGet("/", () => "Hello World!");
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Home}");
+
+            app.Run();
+        }
+    }
+}
